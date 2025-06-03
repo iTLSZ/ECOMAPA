9c8e4eb0da74c9866074804522d1f9cafee92193
iTLZS <lalopezsuarez@uniguajira.edu.co>
2025-06-03 15:30:53 -0500

    aAApP

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import './globals.css'
 
 export const metadata: Metadata = {
   title: 'MAPA DE REDES',
-  description: 'Ecomapa Interactivo - JAC Popayán',
+  description: 'Mapa de Redes Interactivo - JAC Popayán',
   generator: '...',
 }
 
